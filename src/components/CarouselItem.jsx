48ed6a9da7463f29bf6396a3eac2492e19ff87e6
Javier <nejhernandezb@correo.udistrital.edu.co>
2021-11-12 14:32:37 -0500

    En progress

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,18 +1,18 @@
+/* eslint-disable camelcase */
 /* eslint-disable import/no-unresolved */
 import React from 'react';
 import '@styles/components/CarouselItem.scss';
-import imagen from '@images/pexels-ena-marinkovic-3640930.jpg';
 
-const CarouselItem = () => (
+const CarouselItem = ({ backdrop_path, original_title, release_date }) => (
   <div className='carousel-item'>
-    <img className='carousel-item__img' src={imagen} alt='img' />
+    <img className='carousel-item__img' src={`https://image.tmdb.org/t/p/w342${backdrop_path}`} alt='img' />
     <div className='carousel-item__details'>
       <div>
         <img className='carousel-item__details--img' src='https://img.icons8.com/flat-round/64/000000/play--v1.png' alt='' />
         <img className='carousel-item__details--img' src='https://img.icons8.com/flat-round/64/000000/plus.png' alt='' />
       </div>
-      <p className='carousel-item__details--title'>Titulo descriptivo</p>
-      <p className='carousel-item__details--subtitle'>2021 16+ 115 min</p>
+      <p className='carousel-item__details--title'>{original_title}</p>
+      <p className='carousel-item__details--subtitle'>{release_date}</p>
     </div>
   </div>
 );
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable import/no-unresolved */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '@styles/App.scss';
 import Header from '@components/Header';
 import Search from '@components/Search';
@@ -8,30 +9,39 @@ import Carousel from '@components/Carousel';
 import CarouselItem from '@components/CarouselItem';
 import Footer from '@components/Footer';
 
-const App = () => (
-  <div className='app'>
-    <Header />
-    <Search />
-    <Categories title='My list'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Categories title='Film premieres'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Footer />
-  </div>
-);
+const KEYAPI = 'c7dcdd10534cce4ba270cb140c089255';
+
+const App = () => {
+  const [films, setFilms] = useState([]);
+
+  useEffect(() => {
+    const fetchFilms = async () => {
+      try {
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${KEYAPI}`);
+        const data = await response.json();
+        return setFilms(data.results);
+      } catch {
+        console.log(error);
+      }
+    };
+    fetchFilms();
+  }, []);
+
+  console.log(films);
+  console.log(KEYAPI);
+
+  return (
+    <div className='app'>
+      <Header />
+      <Search />
+      <Categories title='Film premieres'>
+        <Carousel>
+          {films.map((item) => <CarouselItem key={item.id} {...item} />)}
+        </Carousel>
+      </Categories>
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
